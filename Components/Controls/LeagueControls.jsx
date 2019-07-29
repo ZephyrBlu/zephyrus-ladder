082be079576b2d2267e48675bfab5d8d76643112
ZephyrBlu <lukejholroyd@gmail.com>
2019-07-29 22:53:51 +1200

    Refactor LeagueControls from Class to Functional component

--- Components/Controls/LeagueControls.jsx
+++ Components/Controls/LeagueControls.jsx
@@ -2,63 +2,51 @@ import { Component, Fragment } from 'react';
 import './LeagueControls.css';
 import './Controls.css';
 
-export default class LeagueControls extends Component {
-    static defaultProps = {
-        onLeagueChange: () => {},
-        currentLeague: 'All',
-    }
+const LeagueControls = (props) => {
+    const handleLeagueChange = (selectedLeague) => {
+        props.onLeagueChange(selectedLeague);
+    };
 
-    constructor(props) {
-        super(props);
-
-        this.isLeagueActive = this.isLeagueActive.bind(this);
-        this.handleLeagueChange = this.handleLeagueChange.bind(this);
-    }
-
-    handleLeagueChange(selectedLeague) {
-        this.props.onLeagueChange(selectedLeague);
-    }
-
-    isLeagueActive(league) {
-        if (this.props.currentLeague === league) {
+    const isLeagueActive = (league) => {
+        if (props.currentLeague === league) {
             return 'active';
         }
         return '';
+    };
+
+    const leagues = [
+        'All',
+        'Grandmaster',
+        'Master',
+        'Diamond',
+        'Platinum',
+        'Gold',
+        'Silver',
+        'Bronze',
+    ];
+
+    switch (props.chart) {
+        case 'winrate':
+        case 'mmr':
+            return (
+                <div id="league" className="controls">
+                    {leagues.map((league, index) => (
+                        <button
+                            key={index}
+                            onClick={() => handleLeagueChange(league)}
+                            className={`${isLeagueActive(league)}`}
+                        >
+                            {league}
+                        </button>
+                    ))}
+                </div>
+            );
+
+        default:
+            return (
+                <p>No chart specified</p>
+            );
     }
+};
 
-    render() {
-        const leagues = [
-            'All',
-            'Grandmaster',
-            'Master',
-            'Diamond',
-            'Platinum',
-            'Gold',
-            'Silver',
-            'Bronze',
-        ];
-
-        switch (this.props.chart) {
-            case 'winrate':
-            case 'mmr':
-                return (
-                    <div id="league" className="controls">
-                        {leagues.map((league, index) => (
-                            <button
-                                key={index}
-                                onClick={() => this.handleLeagueChange(league)}
-                                className={`${this.isLeagueActive(league)}`}
-                            >
-                                {league}
-                            </button>
-                        ))}
-                    </div>
-                );
-
-            default:
-                return (
-                    <p>No chart specified</p>
-                );
-        }
-    }
-}
+export default LeagueControls;
