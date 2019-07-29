2b5335265646a6f093348581deed34b6ff7678d4
ZephyrBlu <lukejholroyd@gmail.com>
2019-07-29 22:50:46 +1200

    Refactor RaceControls from Class to Functional component

--- Components/Controls/RaceControls.jsx
+++ Components/Controls/RaceControls.jsx
@@ -1,54 +1,35 @@
-import { Component, Fragment } from 'react';
+import { useEffect, Fragment } from 'react';
 import './RaceControls.css';
 import './Controls.css';
 
-export default class RaceControls extends Component {
-    static defaultProps = {
-        onRaceSelect: () => {},
-        isLineActive: () => {},
-        lineState: {
-            All: 1,
-            Protoss: 0,
-            Terran: 0,
-            Zerg: 0,
-            Random: 0,
-        },
+const RaceControls = (props) => {
+    const handleRaceSelect = (selectedRace) => {
+        props.onRaceSelect(selectedRace);
+    };
+
+    const races = [
+        'All',
+        'Protoss',
+        'Terran',
+        'Zerg',
+        'Random',
+    ];
+
+    if (props.chart === 'mmr') {
+        return (
+            <div id="race" className="controls">
+                {races.map((race, index) => (
+                    <button
+                        key={index}
+                        onClick={() => handleRaceSelect(race)}
+                        className={`${props.isLineActive(race)}`}
+                    >
+                        {race}
+                    </button>
+                ))}
+            </div>
+        );
     }
+};
 
-    constructor(props) {
-        super(props);
-
-        this.isLineActive = props.isLineActive.bind(this);
-        this.handleRaceSelect = this.handleRaceSelect.bind(this);
-    }
-
-    handleRaceSelect(selectedRace) {
-        this.props.onRaceSelect(selectedRace);
-    }
-
-    render() {
-        const races = [
-            'All',
-            'Protoss',
-            'Terran',
-            'Zerg',
-            'Random',
-        ];
-
-        if (this.props.chart === 'mmr') {
-            return (
-                <div id="race" className="controls">
-                    {races.map((race, index) => (
-                        <button
-                            key={index}
-                            onClick={() => this.handleRaceSelect(race)}
-                            className={`${this.isLineActive(race)}`}
-                        >
-                            {race}
-                        </button>
-                    ))}
-                </div>
-            );
-        }
-    }
-}
+export default RaceControls;
