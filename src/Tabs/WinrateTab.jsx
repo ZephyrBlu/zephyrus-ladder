b16c2750da6188d0519952bc2c0ecc7e56dfedb7
ZephyrBlu <lukejholroyd@gmail.com>
2019-08-03 21:52:19 +1200

    Fix issues with weekly chart

--- src/Tabs/WinrateTab.jsx
+++ src/Tabs/WinrateTab.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import {
+    ResponsiveContainer,
     LineChart,
     Line,
     XAxis,
@@ -10,6 +11,8 @@ import {
 import Tippy from '@tippy.js/react';
 import LeagueControls from '../Components/Controls/LeagueControls';
 import './CSS/WinrateTab.css';
+import './CSS/WinrateTab-Grid.css';
+import './CSS/WinrateTab-Weekly.css';
 
 const WinrateTab = (props) => {
     const [dataType, setDataType] = useState({
@@ -21,6 +24,7 @@ const WinrateTab = (props) => {
         chart: 'Grandmaster',
     });
     const [currentMatchup, setCurrentMatchup] = useState('PvP');
+    const [activeChart, setActiveChart] = useState('grid');
 
     const handleLeagueChange = (selectedLeague, type) => {
         setCurrentLeague(prevState => ({
@@ -41,6 +45,10 @@ const WinrateTab = (props) => {
                 [type]: 'all',
             }));
         }
+    };
+
+    const changeChart = (chart) => {
+        setActiveChart(chart);
     };
 
     const races = ['Protoss', 'Terran', 'Zerg', 'Random'];
@@ -117,176 +125,211 @@ const WinrateTab = (props) => {
         return '';
     };
 
-    return (
-        <section id="winrate">
-            <section id="winrate-monthly">
-                <span id="winrate-monthly-title">
-                    <h2>Monthly Winrates</h2>
-                    <Tippy
-                        content={monthlyContent}
-                        size="regular"
-                        arrow="true"
-                    >
-                        <svg
-                            id="winrate-monthly-help"
-                            xmlns="http://www.w3.org/2000/svg"
-                            x="0px"
-                            y="0px"
-                            width="25"
-                            height="25"
-                            viewBox="0 0 172 172"
-                            style={{
-                                fill: '#000000',
-                                position: 'relative',
-                                top: '5px',
-                                right: '-5px',
-                            }}
+    let chart;
+
+    switch (activeChart) {
+        case 'grid':
+            chart = (
+                <section id="winrate-monthly">
+                    <span id="winrate-monthly-title">
+                        <h2>Monthly Winrates</h2>
+                        <Tippy
+                            content={monthlyContent}
+                            size="regular"
+                            arrow="true"
                         >
-                            <g
-                                fill="none"
-                                fillRule="nonzero"
-                                stroke="none"
-                                strokeWidth="1"
-                                strokeLinecap="butt"
-                                strokeLinejoin="miter"
-                                strokeMiterlimit="10"
-                                strokeDasharray=""
-                                strokeDashoffset="0"
-                                fontFamily="none"
-                                fontWeight="none"
-                                fontSize="none"
-                                textAnchor="none"
-                                style={{ mixBlendMode: 'normal' }}
+                            <svg
+                                id="winrate-monthly-help"
+                                xmlns="http://www.w3.org/2000/svg"
+                                x="0px"
+                                y="0px"
+                                width="25"
+                                height="25"
+                                viewBox="0 0 172 172"
+                                style={{
+                                    fill: '#000000',
+                                    position: 'relative',
+                                    top: '5px',
+                                    right: '-5px',
+                                }}
                             >
-                                <path
-                                    d="M0,172v-172h172v172z"
+                                <g
                                     fill="none"
-                                />
-                                <g fill="#787878">
-                                    <g id="surface1">
-                                        <path
-                                            d="M86,21.5c-35.56738,0 -64.5,28.93262 -64.5,64.5c0,35.56739 28.93262,64.5 64.5,64.5c35.56739,0 64.5,-28.93261 64.5,-64.5c0,-35.56738 -28.93261,-64.5 -64.5,-64.5zM86,32.25c29.75146,0 53.75,23.99854 53.75,53.75c0,29.75146 -23.99854,53.75 -53.75,53.75c-29.75146,0 -53.75,-23.99854 -53.75,-53.75c0,-29.75146 23.99854,-53.75 53.75,-53.75zM86,53.75c-11.8208,0 -21.5,9.6792 -21.5,21.5h10.75c0,-6.00488 4.74512,-10.75 10.75,-10.75c6.00489,0 10.75,4.74512 10.75,10.75c0,4.11523 -2.64551,7.76856 -6.55078,9.07031l-2.18359,0.67188c-4.38818,1.44873 -7.39062,5.64795 -7.39062,10.24609v6.88672h10.75v-6.88672l2.18359,-0.67187c8.27246,-2.75049 13.94141,-10.60303 13.94141,-19.31641c0,-11.8208 -9.6792,-21.5 -21.5,-21.5zM80.625,107.5v10.75h10.75v-10.75z"
-                                        />
+                                    fillRule="nonzero"
+                                    stroke="none"
+                                    strokeWidth="1"
+                                    strokeLinecap="butt"
+                                    strokeLinejoin="miter"
+                                    strokeMiterlimit="10"
+                                    strokeDasharray=""
+                                    strokeDashoffset="0"
+                                    fontFamily="none"
+                                    fontWeight="none"
+                                    fontSize="none"
+                                    textAnchor="none"
+                                    style={{ mixBlendMode: 'normal' }}
+                                >
+                                    <path
+                                        d="M0,172v-172h172v172z"
+                                        fill="none"
+                                    />
+                                    <g fill="#787878">
+                                        <g id="surface1">
+                                            <path
+                                                d="M86,21.5c-35.56738,0 -64.5,28.93262 -64.5,64.5c0,35.56739 28.93262,64.5 64.5,64.5c35.56739,0 64.5,-28.93261 64.5,-64.5c0,-35.56738 -28.93261,-64.5 -64.5,-64.5zM86,32.25c29.75146,0 53.75,23.99854 53.75,53.75c0,29.75146 -23.99854,53.75 -53.75,53.75c-29.75146,0 -53.75,-23.99854 -53.75,-53.75c0,-29.75146 23.99854,-53.75 53.75,-53.75zM86,53.75c-11.8208,0 -21.5,9.6792 -21.5,21.5h10.75c0,-6.00488 4.74512,-10.75 10.75,-10.75c6.00489,0 10.75,4.74512 10.75,10.75c0,4.11523 -2.64551,7.76856 -6.55078,9.07031l-2.18359,0.67188c-4.38818,1.44873 -7.39062,5.64795 -7.39062,10.24609v6.88672h10.75v-6.88672l2.18359,-0.67187c8.27246,-2.75049 13.94141,-10.60303 13.94141,-19.31641c0,-11.8208 -9.6792,-21.5 -21.5,-21.5zM80.625,107.5v10.75h10.75v-10.75z"
+                                            />
+                                        </g>
                                     </g>
                                 </g>
-                            </g>
-                        </svg>
-                    </Tippy>
-                </span>
-                <table>
-                    <tbody>
-                        <tr>
-                            <td id="league">
-                                {currentLeague.grid}
-                            </td>
-                            {races.map(race => (
-                                <td key={`${race}-horiz`} className="winrate-heading-horiz">
-                                    {race}
+                            </svg>
+                        </Tippy>
+                    </span>
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td id="league">
+                                    {currentLeague.grid}
                                 </td>
-                            ))}
-                        </tr>
-                        {races.map(race => (
-                            <tr key={`${race}-row`}>
-                                <td key={`${race}-vert`} className="winrate-heading-vert">
-                                    {race}
-                                </td>
-                                {races.map(innerRace => (
-                                    <Tippy
-                                        key={`tippy-${race}-${innerRace}`}
-                                        content={
-                                            `${race[0]}v${innerRace[0]}:
-                                            ${currentAllData[`${race[0]}v${innerRace[0]}`][0]}%
-                                            (${currentAllData[`${race[0]}v${innerRace[0]}`][1]})`
-                                        }
-                                        arrow="true"
-                                    >
-                                        <td
-                                            key={`${race}-value-vert`}
-                                            className={
-                                                `${checkDataType(race, innerRace, 'grid')} values`
-                                            }
-                                            style={{
-                                                backgroundColor: getColour(currentAllData[`${race[0]}v${innerRace[0]}`][0]),
-                                            }}
-                                        />
-                                    </Tippy>
+                                {races.map(race => (
+                                    <td key={`${race}-horiz`} className="winrate-heading-horiz">
+                                        {race}
+                                    </td>
                                 ))}
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                            {races.map(race => (
+                                <tr key={`${race}-row`}>
+                                    <td key={`${race}-vert`} className="winrate-heading-vert">
+                                        {race}
+                                    </td>
+                                    {races.map(innerRace => (
+                                        <Tippy
+                                            key={`tippy-${race}-${innerRace}`}
+                                            content={
+                                                `${race[0]}v${innerRace[0]}:
+                                                ${currentAllData[`${race[0]}v${innerRace[0]}`][0]}%
+                                                (${currentAllData[`${race[0]}v${innerRace[0]}`][1]})`
+                                            }
+                                            arrow="true"
+                                            size="large"
+                                        >
+                                            <td
+                                                key={`${race}-value-vert`}
+                                                className={
+                                                    `${checkDataType(race, innerRace, 'grid')} values`
+                                                }
+                                                style={{
+                                                    backgroundColor: getColour(currentAllData[`${race[0]}v${innerRace[0]}`][0]),
+                                                }}
+                                            />
+                                        </Tippy>
+                                    ))}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
 
-                <span className="all-controls">
-                    <Tippy
-                        content={`Counts all matches played by ${currentLeague.grid} players`}
-                        arrow="true"
-                    >
-                        <h4>All</h4>
-                    </Tippy>
-                    <Tippy
-                        content={`Only counts matches played between 2 ${currentLeague.grid} players`}
-                        arrow="true"
-                    >
-                        <h4>League</h4>
-                    </Tippy>
-                    <label className="switch">
-                        <input type="checkbox" onClick={() => changeDataType('grid')} />
-                        <span className="slider round" />
-                    </label>
-                    <LeagueControls
-                        id="league"
-                        chart="grid"
-                        leagueKey="grid"
-                        onLeagueChange={handleLeagueChange}
-                        currentLeague={currentLeague}
-                    />
-                </span>
-            </section>
+                    <span className="all-controls">
+                        <Tippy
+                            content={`Counts all matches played by ${currentLeague.grid} players`}
+                            arrow="true"
+                        >
+                            <h4>All</h4>
+                        </Tippy>
+                        <Tippy
+                            content={`Only counts matches played between 2 ${currentLeague.grid} players`}
+                            arrow="true"
+                        >
+                            <h4>League</h4>
+                        </Tippy>
+                        <label className="switch">
+                            <input type="checkbox" onClick={() => changeDataType('grid')} />
+                            <span className="slider round" />
+                        </label>
+                        <LeagueControls
+                            id="league"
+                            chart="grid"
+                            leagueKey="grid"
+                            onLeagueChange={handleLeagueChange}
+                            currentLeague={currentLeague}
+                        />
+                    </span>
+                </section>
+            );
+            break;
 
-            <section id="winrate-weekly">
-                <LineChart width={600} height={300} data={currentWeeklyData}>
-                    <XAxis dataKey="bin" />
-                    <YAxis />
-                    <CartesianGrid horizontal={false} vertical={false} />
-                    <Tooltip />
-                    {races.map(race => (
-                        races.map(innerRace => (
-                            <Line
-                                key={`winrate-weekly-${race}`}
-                                type="monotone"
-                                dataKey={`${race[0]}v${innerRace[0]}.value[0]`}
-                                stroke={raceColours[`${race}`]}
+        case 'chart':
+            chart = (
+                <section id="winrate-weekly">
+                    <ResponsiveContainer width="99%" height={600}>
+                        <LineChart data={currentWeeklyData}>
+                            <XAxis dataKey="bin" />
+                            <YAxis />
+                            <CartesianGrid horizontal={false} vertical={false} />
+                            <Tooltip
+                                formatter={(value, name, payload) => ([`${value}%`, `${payload.dataKey.slice(0, 3)}`])}
                             />
-                        ))
-                    ))}
-                </LineChart>
+                            {races.map(race => (
+                                races.map(innerRace => (
+                                    <Line
+                                        key={`winrate-weekly-${race}-${innerRace}`}
+                                        type="monotone"
+                                        dataKey={`${race[0]}v${innerRace[0]}.value[0]`}
+                                        stroke={raceColours[`${race}`]}
+                                        dot={false}
+                                    />
+                                ))
+                            ))}
+                        </LineChart>
+                    </ResponsiveContainer>
 
-                <p style={{ color: 'white' }}>{currentMatchup}</p>
+                    <span className="all-controls">
+                        <Tippy
+                            content={`Counts all matches played by ${currentLeague.grid} players`}
+                            arrow="true"
+                        >
+                            <h4>All</h4>
+                        </Tippy>
+                        <Tippy
+                            content={`Only counts matches played between 2 ${currentLeague.grid} players`}
+                            arrow="true"
+                        >
+                            <h4>League</h4>
+                        </Tippy>
+                        <label className="switch">
+                            <input type="checkbox" onClick={() => changeDataType('grid')} />
+                            <span className="slider round" />
+                        </label>
+                        <LeagueControls
+                            id="league"
+                            chart="chart"
+                            leagueKey="chart"
+                            onLeagueChange={handleLeagueChange}
+                            currentLeague={currentLeague}
+                        />
+                    </span>
+                </section>
+            );
+            break;
 
-                <Tippy
-                    content={`Counts all matches played by ${currentLeague.chart} players`}
-                    arrow="true"
+        default:
+            break;
+    }
+
+    return (
+        <section id="winrate">
+            <nav>
+                <button
+                    onClick={() => changeChart('grid')}
                 >
-                    <h4 style={{ color: 'white' }}>All</h4>
-                </Tippy>
-                <Tippy
-                    content={`Only counts matches played between 2 ${currentLeague.chart} players`}
-                    arrow="true"
+                    Grid
+                </button>
+                <button
+                    onClick={() => changeChart('chart')}
                 >
-                    <h4 style={{ color: 'white' }}>League</h4>
-                </Tippy>
-                <label className="switch">
-                    <input type="checkbox" onClick={() => changeDataType('chart')} />
-                    <span className="slider round" />
-                </label>
-                <LeagueControls
-                    // id="league"
-                    chart="chart"
-                    leagueKey="chart"
-                    onLeagueChange={handleLeagueChange}
-                    currentLeague={currentLeague}
-                />
-            </section>
+                    Timeline
+                </button>
+            </nav>
+            {chart}
         </section>
     );
 };
